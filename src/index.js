86772a8e9a02fc65f7c85e36c642554d6f5603eb
Antonin Monmarthe <antonin.monmarthe@googlemail.com>
2016-06-13 17:28:44 +0200

    Commit init 2 test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 import angular from 'angular'
+import PizzaModule from './pizza'
 
-angular.module('dtang', [])
-  .config(function () {
-    console.log('Config coucou')
-  })
-  .value('AppName', 'DTANG')
-  .run(function (AppName) {
-    console.log(AppName + ' : Run coucou')
-  })
-  .controller('monControleur', monControleur)
+angular.module('dtang', [
+  PizzaModule
+])
 
 angular.bootstrap(document, ['dtang'])
-
-function monControleur () {
-  const monCtrl = this
-  monCtrl.page = '1'
-  console.log('Mon controleur')
-
-
-  monCtrl.click = function click (page) {
-    monCtrl.page = page
-  }
-  monCtrl.message = 'Coucou : '
-
-  monCtrl.user = {
-    name: 'Antonin'
-  }
-
-  monCtrl.getName = function () {
-    return monCtrl.user.name
-  }
-}
